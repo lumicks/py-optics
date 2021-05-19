Fix issue that moving the bead also moved the focus (sign issue kx and ky)
commit 510197247381de30bd2945c803fda2c342711cad
Author: Robert Moerland <r.moerland@lumicks.com>
Date:   Wed May 19 12:02:48 2021 +0200

    Add test that shifting the bead does not shift the focus position

commit 7bac974234ea311cb2fa7c825be058801d1d0862
Author: Robert Moerland <r.moerland@lumicks.com>
Date:   Mon May 17 17:47:11 2021 +0200

    change sign on Kx and Ky

src/pyoptics/mie_calc/__init__.py
@@ -300,8 +300,8 @@ class MieCalc:
         # Calculate properties of the plane waves
         self._Kz = self._k * np.cos(self._Th)
         self._Kp = self._k * np.sin(self._Th)
-        self._Kx = self._Kp * np.cos(self._Phi)
-        self._Ky = self._Kp * np.sin(self._Phi)
+        self._Kx = -self._Kp * np.cos(self._Phi)
+        self._Ky = -self._Kp * np.sin(self._Phi)
 
         Einx, Einy = f_input_field(X_BFP, Y_BFP, R_BFP, Rmax, self._Th, self._Phi)
         

tests/mie_calc/test_mie_calc_psf.py
@@ -49,5 +49,34 @@ def test_gaussian_input(
     #return [Exm, Eym, Ezm, Exr, Eyr, Ezr, Exf, Eyf, Ezf]
     np.testing.assert_allclose([Exm, Eym, Ezm], [Exr, Eyr, Ezr], rtol=1e-8, atol=1e-8)
     np.testing.assert_allclose([Exf, Eyf, Ezf], [Exr, Eyr, Ezr], rtol=1e-8, atol=1e-8)
-    
 
+
+
+# %%
+@pytest.mark.parametrize("bead_center", [(1e-6, 0.4e-6, -0.5e-6),(-0.2e-6, -0.33e-6, 0.554e-6)])
+def test_gaussian_input_bead_shift(bead_center):
+    bead_size = 1e-9
+    lambda_vac = 1.064e-6
+    n_bead =  1.5
+    num_pts = 21
+    NA = 1.2
+    n_medium=1.5
+    focal_length = 4.43e-3
+    z_eval = 0  # np.linspace(-2 * lambda_vac, 2 * lambda_vac, num_pts)
+    xy_eval = np.linspace(-2 * lambda_vac, 2 * lambda_vac, num_pts)
+    mie = mc.MieCalc(bead_size, n_bead, n_medium, lambda_vac)
+    bfp_sampling = 31
+    
+    Exm, Eym, Ezm, X, Y, Z = mie.fields_gaussian_focus(n_BFP=1.0, focal_length=focal_length, NA=NA, filling_factor=1.0,x=xy_eval, y=xy_eval, z=z_eval, 
+                                         bead_center=(0,0,0), bfp_sampling_n=bfp_sampling, return_grid=True, verbose=True, num_orders=None)
+    Exr, Eyr, Ezr = psf.focused_gauss(lambda_vac, n_bfp=1.0, n_medium=n_medium, focal_length=focal_length, filling_factor=1.0,
+        NA=NA, x=xy_eval, y=xy_eval, z=z_eval, bfp_sampling_n=bfp_sampling, return_grid=False)
+    
+    Exf, Eyf, Ezf = psf.fast_gauss_psf(lambda_vac, n_bfp=1.0, n_medium=n_medium, focal_length=focal_length, filling_factor=1.0,
+        NA=NA, xrange=4*lambda_vac, numpoints_x=num_pts, yrange=4*lambda_vac, numpoints_y=num_pts, z=z_eval, bfp_sampling_n=bfp_sampling, return_grid=False)
+    
+    #return [Exm, Eym, Ezm, Exr, Eyr, Ezr, Exf, Eyf, Ezf]
+    np.testing.assert_allclose([Exm, Eym, Ezm], [Exr, Eyr, Ezr], rtol=1e-8, atol=1e-8)
+    np.testing.assert_allclose([Exf, Eyf, Ezf], [Exr, Eyr, Ezr], rtol=1e-8, atol=1e-8)
+
+# %%