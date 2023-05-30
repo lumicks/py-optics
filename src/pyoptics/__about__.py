af8535b7d023dddb712217dd28166e407b0e2825
Robert Moerland <r.moerland@lumicks.com>
2023-05-31 00:33:24 +0200

    add __about__

---
+++ src/pyoptics/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'pyoptics'
+__version__ = '0.4.0'
+__summary__ = 'Computational code for nanophotonics'
+__url__ = 'https://github.com/lumicks/py-optics'
+
+__author__ = "Lumicks B.V."
+__copyright__ = "2023, " + __author__
+__email__ = "devteam@lumicks.com"
+__license__ = "Apache 2.0"
--- src/pyoptics/__init__.py
+++ src/pyoptics/__init__.py
@@ -1,9 +1,11 @@
-__title__ = 'pyoptics'
-__version__ = '0.4.0'
-
-__description__ = 'Computational code for nanophotonics'
-__url__ = 'https://github.com/lumicks/py-optics'
-
-__author__ = 'Robert J. Moerland'
-__email__ = 'r.moerland@lumicks.com'
-__copyright__ = 'Copyright (c) 2021 - 2023 Robert J. Moerland, LUMICKS B.V.'
+from .__about__ import (
+    __author__,
+    __copyright__,
+    __doc__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __url__,
+    __version__,
+)
