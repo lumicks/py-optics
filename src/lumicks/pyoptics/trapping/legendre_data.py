02423e2d08810e314e647120e85aaf967f0bdbaf
Robert Moerland <r.moerland@lumicks.com>
2023-08-08 22:14:41 +0200

    rename variables

--- src/lumicks/pyoptics/trapping/legendre_data.py
+++ src/lumicks/pyoptics/trapping/legendre_data.py
@@ -1,10 +1,9 @@
 import numpy as np
 from numba import njit, prange
-from .associated_legendre import (
-    associated_legendre,
-    associated_legendre_dtheta,
-    associated_legendre_over_sin_theta
-)
+from dataclasses import fields
+from .associated_legendre import associated_legendre_dtheta, associated_legendre_over_sin_theta
+from .local_coordinates import CoordLocation, LocalBeadCoordinates
+from .farfield_data import FarfieldData
 
 
 @njit(cache=True)
@@ -18,10 +17,10 @@ def _exec_lookup(source, lookup, r, c):
 
 class AssociatedLegendreData:
     __slots__ = (
-        '_associated_legendre',
-        '_associated_legendre_over_sin_theta',
-        '_associated_legendre_dtheta',
-        '_inverse'
+        "_associated_legendre",
+        "_associated_legendre_over_sin_theta",
+        "_associated_legendre_dtheta",
+        "_inverse",
     )
 
     def __init__(
@@ -31,10 +30,8 @@ class AssociatedLegendreData:
         associated_legendre_dtheta: np.ndarray,
         inverse: np.ndarray,
     ) -> None:
-
         self._associated_legendre = associated_legendre
-        self._associated_legendre_over_sin_theta = \
-            associated_legendre_over_sin_theta
+        self._associated_legendre_over_sin_theta = associated_legendre_over_sin_theta
         self._associated_legendre_dtheta = associated_legendre_dtheta
         self._inverse = inverse
 
@@ -42,65 +39,70 @@ class AssociatedLegendreData:
         return _exec_lookup(self._associated_legendre, self._inverse, r, c)
 
     def associated_legendre_over_sin_theta(self, r: int, c: int):
-        return _exec_lookup(
-            self._associated_legendre_over_sin_theta, self._inverse, r, c
-        )
+        return _exec_lookup(self._associated_legendre_over_sin_theta, self._inverse, r, c)
 
     def associated_legendre_dtheta(self, r: int, c: int):
-        return _exec_lookup(
-            self._associated_legendre_dtheta, self._inverse, r, c
-        )
+        return _exec_lookup(self._associated_legendre_dtheta, self._inverse, r, c)
 
 
 @njit(cache=True, parallel=True)
 def _loop_over_rotations(
-    local_coords: np.ndarray, radii: np.ndarray, aperture: np.ndarray,
-    cos_theta: np.ndarray, sin_theta: np.ndarray,
-    cos_phi: np.ndarray, sin_phi: np.ndarray
+    local_coords_stacked: np.ndarray,
+    radii: np.ndarray,
+    aperture: np.ndarray,
+    cos_theta: np.ndarray,
+    sin_theta: np.ndarray,
+    cos_phi: np.ndarray,
+    sin_phi: np.ndarray,
 ):
     """
-    Loop over all possible rotations, in order to find the unique values of
-    cos(theta) to reduce the computational load calculating Legendre
-    polynomials.
+    Find all possible values of cos(theta), where theta is the angle of a coordinate with the z
+    axis. Do this by looping over all possible rotations of the local coordinate system, as dictated
+    by the plane waves in the focus, and calculating cos(theta) for every coordinate and every
+    rotation.
     """
     rows, cols = np.nonzero(aperture)
-    
+
     # Weird construct to work around tuple unpacking bug in Numba
     # https://github.com/numba/numba/issues/8772
     shape = (aperture.shape[0], aperture.shape[1], radii.size)
-    cosTs = np.zeros(shape)
+    local_cos_theta = np.zeros(shape)
 
     index = radii == 0
     for r, c in zip(rows, cols):
-        # this line is a workaround for a TypingError in Numba:
-        ct = cosTs[r, c, :]
 
         # Rotate the coordinate system such that the x-polarization on the
         # bead coincides with theta polarization in global space
         # however, cos(theta) is the same for phi polarization!
-        # A = (_R_th(cos_theta[p,m], sin_theta[p,m]) @
-        #     _R_phi(cos_phi[p,m], -sin_phi[p,m]))
-        # coords = A @ local_coords
-        # z = coords[2, :]
+        # >>> A = (_R_th(cos_theta_bfp[p,m], sin_theta[p,m]) @
+        # >>>     _R_phi(cos_phi[p,m], -sin_phi[p,m]))
+        # >>> coords = A @ local_coords_stacked
+        # >>> z = coords[2, :]
+
+        # The following line is equal to z = coords[2, :] after doing the transform with A
         z = (
-            local_coords[2, :] * cos_theta[r, c] - (
-                local_coords[0, :] * cos_phi[r, c] +
-                local_coords[1, :] * sin_phi[r, c]
-            ) * sin_theta[r, c]
+            local_coords_stacked[2, :] * cos_theta[r, c]
+            - (
+                local_coords_stacked[0, :] * cos_phi[r, c]
+                + local_coords_stacked[1, :] * sin_phi[r, c]
+            )
+            * sin_theta[r, c]
         )
 
         # Retrieve an array of all values of cos(theta)
-        ct[:] = z / radii  # cos(theta)
-        ct[index] = 1
-        cosTs[r, c, :] = ct
+        local_cos_theta[r, c, :] = z / radii  # cos(theta)
+        local_cos_theta[r, c, index] = 1
 
-    return cosTs
+    return local_cos_theta
 
 
 @njit(cache=True, parallel=True)
 def _do_legendre_calc(
-    cosT_unique: np.ndarray, abs_cosT_unique: np.ndarray,
-    sign_inv: np.ndarray, sign_cosT_unique, n_orders: int
+    unique_cos_theta: np.ndarray,
+    unique_abs_cos_theta: np.ndarray,
+    sign_inv: np.ndarray,
+    max_negative_index: int,
+    n_orders: int,
 ):
     """
     Numba compatible function that computes Associated Legendre Polynomials for
@@ -113,62 +115,68 @@ def _do_legendre_calc(
     alp_sin = np.zeros_like(alp)
 
     # Parity for order L: parity[0] == 1, for L == 1
-    parity = (-1)**np.arange(n_orders)
+    parity = (-1) ** np.arange(n_orders)
+    sin_theta = (((1 + unique_abs_cos_theta) * (1 - unique_abs_cos_theta)) ** 0.5)[sign_inv]
     for L in prange(1, n_orders + 1):
-        alp[L - 1, :] = associated_legendre(L, abs_cosT_unique)[sign_inv]
-        if parity[L - 1] == -1:
-            alp[L - 1, :] *= sign_cosT_unique
-        alp_sin[L - 1, :] = associated_legendre_over_sin_theta(
-            L, cosT_unique, alp[L - 1, :]
-        )
+        alp_sin[L - 1, :] = associated_legendre_over_sin_theta(L, unique_abs_cos_theta)[sign_inv]
+        alp[L - 1, :] = alp_sin[L - 1, :] * sin_theta
 
-    # For some reason, prange leads to numerical errors here
-    for L in range(1, n_orders + 1):
-        alp_prev = alp[L - 2, :] if L > 1 else None
+        if parity[L - 1] == -1 and max_negative_index is not None:
+            alp[L - 1, : max_negative_index + 1] *= -1
+            alp_sin[L - 1, : max_negative_index + 1] *= -1
+
+    for L in prange(1, n_orders + 1):
+        alp_prev = alp_sin[L - 2, :] if L > 1 else None
         alp_deriv[L - 1, :] = associated_legendre_dtheta(
-            L, cosT_unique, (alp[L - 1, :], alp_prev)
+            L, unique_cos_theta, (alp_sin[L - 1, :], alp_prev)
         )
 
     return alp, alp_sin, alp_deriv
 
 
 def calculate_legendre(
-    local_coords: np.ndarray, radii: np.ndarray, aperture: np.ndarray,
-    cos_theta: np.ndarray, sin_theta: np.ndarray,
-    cos_phi: np.ndarray, sin_phi: np.ndarray,
-    n_orders: int
+    location: CoordLocation,
+    local_coordinates: LocalBeadCoordinates,
+    farfield_data: FarfieldData,
+    n_orders: int,
 ):
     """
-    Find the value of cos(theta) for all coordinates (x, y, z) after rotating
+    Find the value of cos(theta) for all local coordinates (x, y, z) after rotating
     the coordinates along all possible directions, defined by the angles in the
     back focal plane.
     """
-    if aperture.shape[0] != aperture.shape[1]:
-        raise ValueError("Aperture matrix must be square")
 
-    cosTs = _loop_over_rotations(
-        local_coords, radii, aperture, cos_theta, sin_theta,
-        cos_phi, sin_phi
-    )
+    # Unpack data class members to a dict for Numba
+    kwargs = {
+        field.name: getattr(farfield_data, field.name)
+        for field in fields(farfield_data)
+        if field.name in ("aperture", "cos_theta", "sin_theta", "cos_phi", "sin_phi")
+    }
+    local_coords_stacked = local_coordinates.xyz_stacked(location)
+    radii = local_coordinates.r(location)
+    local_cos_theta = _loop_over_rotations(local_coords_stacked, radii, **kwargs)
 
-    shape = cosTs.shape
-    cosTs = np.reshape(cosTs, cosTs.size)
+    shape = local_cos_theta.shape
+    local_cos_theta = np.reshape(local_cos_theta, local_cos_theta.size)
     # rounding errors may make cos(theta) > 1 or < -1. Fix it to [-1..1]
-    cosTs[cosTs > 1] = 1
-    cosTs[cosTs < -1] = -1
+    local_cos_theta[local_cos_theta > 1] = 1
+    local_cos_theta[local_cos_theta < -1] = -1
 
     # Get the unique values of cos(theta) in the array
-    cosT_unique, inverse = np.unique(cosTs, return_inverse=True)
+    unique_cos_theta, inverse = np.unique(local_cos_theta, return_inverse=True)
     inverse = np.reshape(inverse, shape)
 
     # Get the signs of the cosines, and use parity to reduce the computational
     # load
-    sign_cosT_unique = np.sign(cosT_unique)
-    abs_cosT_unique, sign_inv = np.unique(
-        np.abs(cosT_unique), return_inverse=True
-    )
+    max_negative_index = None
+    try:
+        max_negative_index = np.argmax(np.where(unique_cos_theta < 0)[0])
+    except ValueError:
+        pass
+
+    unique_abs_cos_theta, sign_inv = np.unique(np.abs(unique_cos_theta), return_inverse=True)
 
     alp, alp_sin, alp_deriv = _do_legendre_calc(
-        cosT_unique, abs_cosT_unique, sign_inv, sign_cosT_unique, n_orders
+        unique_cos_theta, unique_abs_cos_theta, sign_inv, max_negative_index, n_orders
     )
     return AssociatedLegendreData(alp, alp_sin, alp_deriv, inverse)
