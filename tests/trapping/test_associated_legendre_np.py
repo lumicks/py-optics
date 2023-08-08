de6942b77385d8f0042937507968232cf39a8fef
Robert Moerland <r.moerland@lumicks.com>
2023-08-08 22:11:19 +0200

    testing: move `associated_legendre_npp` to test only

--- tests/trapping/test_associated_legendre_np.py
+++ tests/trapping/test_associated_legendre_np.py
@@ -1,9 +1,22 @@
 import pytest
 import numpy as np
+import numpy.polynomial as npp
 from lumicks.pyoptics.trapping.associated_legendre import (
     associated_legendre,
-    associated_legendre_npp
 )
+
+
+def associated_legendre_npp(n, x):
+    """Return the 1st order (m == 1) of the
+    associated Legendre polynomial of degree n, evaluated at x [-1..1]
+
+    This is a reference implementation in numpy, which is more generic and therefore slower.
+    Furthermore, it's not compatible with Numba.
+    """
+    orders = np.zeros(n + 1)
+    orders[n] = 1
+    c = npp.Legendre(npp.legendre.legder(orders))
+    return -c(x) * np.sqrt(1 - x**2)
 
 
 @pytest.mark.parametrize('order', range(1, 200))
