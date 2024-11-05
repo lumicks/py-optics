32e234acc6a23bb9c030260b91281b59a17fe657
Robert Moerland <r.moerland@lumicks.com>
2024-11-05 12:20:47 +0100

    update version and year

--- src/lumicks/pyoptics/__about__.py
+++ src/lumicks/pyoptics/__about__.py
@@ -1,9 +1,9 @@
 __title__ = "lumicks.pyoptics"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __summary__ = "Computational code for nanophotonics"
 __url__ = "https://github.com/lumicks/py-optics"
 
 __author__ = "Lumicks B.V."
-__copyright__ = "2023, " + __author__
+__copyright__ = "2024, " + __author__
 __email__ = "devteam@lumicks.com"
 __license__ = "Apache 2.0"
