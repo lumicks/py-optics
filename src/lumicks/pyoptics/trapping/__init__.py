dbc6df5d5885129c6034c0bd161d9136b68aed46
Robert Moerland <r.moerland@lumicks.com>
2024-11-15 00:20:00 +0100

    isort

--- src/lumicks/pyoptics/trapping/__init__.py
+++ src/lumicks/pyoptics/trapping/__init__.py
@@ -1,11 +1,15 @@
+from numba import config
+
+from ..objective import Objective
 from .bead import Bead
 from .interface import (
+    absorbed_power_focus,
     fields_focus,
     fields_focus_gaussian,
     fields_plane_wave,
-    forces_focus,
     force_factory,
+    forces_focus,
     scattered_power_focus,
-    absorbed_power_focus,
 )
-from ..objective import Objective
+
+config.THREADING_LAYER = "threadsafe"
