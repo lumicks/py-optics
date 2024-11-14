44ac0177b4862683f66a58e41239c633fad57961
Robert Moerland <r.moerland@lumicks.com>
2024-11-15 00:25:25 +0100

    update example

--- examples/trapping_forces.py
+++ examples/trapping_forces.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.7
+#       jupytext_version: 1.16.4
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -20,13 +20,9 @@
 # %matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
-import time, sys  # For progress bar
-from IPython.display import clear_output  # For progress bar
-from matplotlib import rc
 import lumicks.pyoptics.trapping as trp
 from scipy.interpolate import interp1d
 from scipy.constants import epsilon_0, speed_of_light as C
-from tqdm.auto import tqdm
 
 # %% [markdown]
 # ## Definition of coordinate system
@@ -42,15 +38,17 @@ from tqdm.auto import tqdm
 
 # %%
 bead_diameter = 4.4e-6  # [m]
-lambda_vac = 1064e-9    # [m]
-n_bead =  1.57          # [-]
-n_medium = 1.33         # [-]
+lambda_vac = 1064e-9  # [m]
+n_bead = 1.57  # [-]
+n_medium = 1.33  # [-]
 
 # %%
 # instantiate a Bead object
-bead = trp.Bead(bead_diameter=bead_diameter, n_bead=n_bead, n_medium=n_medium, lambda_vac=lambda_vac)
+bead = trp.Bead(
+    bead_diameter=bead_diameter, n_bead=n_bead, n_medium=n_medium, lambda_vac=lambda_vac
+)
 # Tell use how many scattering orders are used according to the formula in literature:
-print(f'Number of scattering orders used by default: {bead.number_of_orders}')
+print(f"Number of scattering orders used by default: {bead.number_of_orders}")
 
 # %% [markdown]
 # ## Properties of the objective
@@ -59,47 +57,47 @@ print(f'Number of scattering orders used by default: {bead.number_of_orders}')
 #     <img src="images/objective.png" width=400>
 #     <figcaption>Fig. 2: Coordinate system and the relation to the properties of the objective (NA, focal length, immersion medium)</figcaption>
 # </figure>
-#                                                                           
+#
 
 # %%
 # objective properties, for water immersion
-NA = 1.2                # [-]
+NA = 1.2  # [-]
 focal_length = 4.43e-3  # [m]
-n_bfp = 1.0             # [-] Other side of the water immersion objective is air
+n_bfp = 1.0  # [-] Other side of the water immersion objective is air
 # Instantiate an Objective. Note that n_medium has to be defined here as well
 objective = trp.Objective(NA=NA, focal_length=focal_length, n_bfp=n_bfp, n_medium=n_medium)
 
 # %% [markdown]
 # ## Properties of the input beam
 #
-# The continuous field distribution at the back focal plane (the input beam, left figure) is discretized with $N$ samples, counting from the center to the extremes of the $x$ and $y$ axes (right figure, here $N=9$). The more samples you have, the more accurate the resulting point spread function will represent the continuous distribution. But, the calculation time will increase with $N^2$. You can get away with relatively small numbers as long as the distance between the bead and the focus is not too large. *Do* check for convergence, however (see below).
+# The continuous field distribution at the back focal plane (the input beam, left figure) is discretized with $(2N-1) \times (2N-1)$ samples, counting $N$ sampels from the center to the extremes of the $x$ and $y$ axes (right figure, here $N=9$). The more samples you have, the more accurate the resulting point spread function will represent the continuous distribution. But, the calculation time will increase with $O(N^2)$. You can get away with relatively small numbers as long as the distance between the bead and the focus is not too large. *Do* check for convergence, however, for real applications (see below).
 # <figure>
 #     <img src="images/aperture_inf.png" width=400 align="left">
 #     <img src="images/aperture_discretized.png" width=400>
 #     <figcaption>Fig. 3: Left - continuous field distribution of the laser beam. Right - discretized approximation</figcaption>
 # </figure>
 #
-# The parameter `bfp_sampling_n` determines the number of samples $N$ in the back focal plane.
+# The parameter `bfp_sampling_n` ($=N$) determines the number of samples in the back focal plane. As an approximation of the focus, a higher number of samples is better but quadratically slower. A too low number causes copies of the point spread function to appear. It is therefore best to check for these artifacts by plotting the fields. In this example, the number here is relatively low ($N=9$) for demonstration purposes.
+#
+# **In general, a convergence check is required to verify the computed results**
+#
 
 # %%
-# approximation of the focus, higher is better and slower (scales with N**2)
-# best to check the correct range by plotting fields!
-# Note that the number here is relatively low for demonstration purposes
-# In general, a convergence check is required to verify the computed results
-bfp_sampling_n=9
+bfp_sampling_n = 9
 
 # 100% is 1.75W into a single trapping beam before the objective, at trap split = 50%
 Pmax = 1.75  # [W]
 power_percentage = 25  # [%]
 
 # %%
-filling_factor = 0.9                                # [-]
+filling_factor = 0.9  # [-]
 w0 = filling_factor * focal_length * NA / n_medium  # [m]
-P = Pmax * power_percentage / 100.                  # [W]
-I0 = 2 * P / (np.pi * w0**2)                        # [W/m^2]
-E0 = (I0 * 2/(epsilon_0 * C * n_bfp))**0.5       # [V/m]
+P = Pmax * power_percentage / 100.0  # [W]
+I0 = 2 * P / (np.pi * w0**2)  # [W/m^2]
+E0 = (I0 * 2 / (epsilon_0 * C * n_bfp)) ** 0.5  # [V/m]
 
-def gaussian_beam(_, x_bfp, y_bfp, *args): 
+
+def gaussian_beam(_, x_bfp, y_bfp, *args):
     Ex = np.exp(-(x_bfp**2 + y_bfp**2) / w0**2) * E0
     return (Ex, None)
 
@@ -107,59 +105,56 @@ def gaussian_beam(_, x_bfp, y_bfp, *args):
 # %% [markdown]
 # ## Forces in the $z$ direction - find the equilibrium
 # Set the range in z to calculate the forces at. We expect the force on the bead in the z-direction to be zero for the interval of $[0, \inf)$
-# Start with a range of $0\ldots 1 \mu m$
+# Start with a range of $0\ldots 1 \mu m$. We will retrieve a function from `force_factory` that we can reuse to calculate the force on a bead at arbitrary locations.
 
 # %%
-z = np.linspace(0, 1e-6, 20)  
-Fz = np.empty(z.shape)
+# Calculate the force at 50 nm intervals
+z = np.linspace(0, 1e-6, 201)
+
+# Obtain a function that returns the force on a bead at a certain coordinate when called.
+force_func = trp.force_factory(gaussian_beam, objective, bead, bfp_sampling_n=bfp_sampling_n)
 
 # %%
-for idx, zz in enumerate(tqdm(z)):
-    F = trp.forces_focus(gaussian_beam, objective, bead, bfp_sampling_n=bfp_sampling_n, bead_center=(0, 0, zz), 
-                                  num_orders=None, integration_orders=None, verbose=False)
-    Fz[idx] = F[2]
+bead_center = [(0, 0, zz) for zz in z]
+Fz = force_func(bead_center)[:, 2]
 
 # %%
 plt.figure(figsize=(8, 6))
 plt.plot(z * 1e9, Fz * 1e12)
-plt.xlabel('$z$ [nm]')
-plt.ylabel('$F$ [pN]')
-plt.title(f'{bead_diameter * 1e6} µm bead, $F_z$ at $x_b = y_b = 0$')
+plt.xlabel("$z$ [nm]")
+plt.ylabel("$F$ [pN]")
+plt.title(f"{bead_diameter * 1e6} µm bead, $F_z$ at $x_b = y_b = 0$")
 plt.show()
 
 # %%
 # linearly interpolate. For better accuracy, you might consider scipy.optimize.brentq
 z_eval = interp1d(Fz, z)(0)
-print(f'Force in z zero near z = {(z_eval*1e9):.1f} nm')
+print(f"Force in z zero near z = {(z_eval*1e9):.1f} nm")
 
 # %% [markdown]
 # ## Forces in $x$ and $y$ directions
 # Calculate the forces in the $x$ and $y$ direction at the location where the force in the $z$ direction is (nearly) zero
 
 # %%
-x = np.linspace(-500e-9, -1e-9, 21)
-Fx = np.empty(x.shape)
-for idx, xx in enumerate(tqdm(x)):
-    F = trp.forces_focus(gaussian_beam, objective, bead, bfp_sampling_n=bfp_sampling_n, 
-                                  bead_center=(xx, 0, z_eval), num_orders=None, integration_orders=None)
-    Fx[idx] = F[0]
+x = np.linspace(-500e-9, 0, 501)
+bead_center = [(xx, 0, z_eval) for xx in x]
+Fx = force_func(bead_center)[:, 0]
 
 # %%
-y = np.linspace(-500e-9, -1e-9, 21)
-Fy = np.empty(y.shape)
-for idx, yy in enumerate(tqdm(y)):
-    F = trp.forces_focus(gaussian_beam, objective, bead, bfp_sampling_n=bfp_sampling_n, 
-                                  bead_center=(0, yy, z_eval), num_orders=None, integration_orders=None)
-    Fy[idx] = F[1]
+y = np.linspace(-500e-9, 0, 501)
+bead_center = [(0, yy, z_eval) for yy in y]
+Fy = force_func(bead_center)[:, 1]
 
 # %%
 plt.figure(figsize=(8, 6))
-plt.plot(x * 1e9,Fx * 1e12, label='x')
-plt.plot(y * 1e9,Fy * 1e12, label='y')
-plt.xlabel('displacement [nm]')
-plt.ylabel('F [pN]')
+plt.plot(x * 1e9, Fx * 1e12, label="x")
+plt.plot(y * 1e9, Fy * 1e12, label="y")
+plt.xlabel("displacement [nm]")
+plt.ylabel("F [pN]")
 plt.legend()
-plt.title(f'{bead_diameter * 1e6} µm bead, $F_x$ at $(x, 0, {z_eval * 1e9:.1f})$ nm, $F_y$ at $(0, y, {z_eval * 1e9:.1f})$ nm')
+plt.title(
+    f"{bead_diameter * 1e6} µm bead, $F_x$ at $(x, 0, {z_eval * 1e9:.1f})$ nm, $F_y$ at $(0, y, {z_eval * 1e9:.1f})$ nm"
+)
 plt.show()
 
 # %% [markdown]
@@ -170,32 +165,41 @@ plt.show()
 # 1. Increase the number of Mie scattering orders and check the difference between the old and newly calculated forces
 
 # %%
-Fz1 = np.empty(z.size)
-Fz2 = np.empty(z.size)
-for idx, k in enumerate(tqdm(z)):
-    F = trp.forces_focus(gaussian_beam, objective, bead=bead, bfp_sampling_n=bfp_sampling_n, 
-                                  bead_center=(0, 0, k), 
-                                  num_orders=None, integration_orders=None, verbose=False)
-    Fz1[idx] = F[2]
-    F = trp.forces_focus(gaussian_beam, objective, bead=bead, bfp_sampling_n=bfp_sampling_n * 2, 
-                                  bead_center=(0, 0, k), 
-                                  num_orders=None, integration_orders=None, verbose=False)
-    Fz2[idx] = F[2]
+Fz1 = trp.forces_focus(
+    gaussian_beam,
+    objective,
+    bead=bead,
+    bfp_sampling_n=bfp_sampling_n,
+    bead_center=[(0, 0, zz) for zz in z],
+    num_orders=None,
+    integration_orders=None,
+    verbose=False,
+)[:, 2]
+Fz2 = trp.forces_focus(
+    gaussian_beam,
+    objective,
+    bead=bead,
+    bfp_sampling_n=bfp_sampling_n * 2,
+    bead_center=[(0, 0, zz) for zz in z],
+    num_orders=None,
+    integration_orders=None,
+    verbose=False,
+)[:, 2]
 
 # %%
 plt.figure(figsize=(12, 4))
 plt.subplot(1, 2, 1)
-plt.plot(z * 1e9, Fz1 * 1e12, label=f'bfp_sampling = {bfp_sampling_n}')
-plt.plot(z * 1e9, Fz2 * 1e12, label=f'bfp_sampling = {bfp_sampling_n * 2}')
-plt.xlabel('z [nm]')
-plt.ylabel('F [pN]')
+plt.plot(z * 1e9, Fz1 * 1e12, label=f"bfp_sampling = {bfp_sampling_n}")
+plt.plot(z * 1e9, Fz2 * 1e12, label=f"bfp_sampling = {bfp_sampling_n * 2}")
+plt.xlabel("z [nm]")
+plt.ylabel("F [pN]")
 plt.legend()
-plt.title(f'{bead_diameter * 1e6} um bead, $F_z$ at X = Y = 0')
+plt.title(f"{bead_diameter * 1e6} um bead, $F_z$ at X = Y = 0")
 plt.subplot(1, 2, 2)
 plt.plot(z * 1e9, (Fz1 - Fz2) * 1e12)
-plt.xlabel('z [nm]')
-plt.ylabel('difference [pN]')
-plt.title('difference between calculations')
+plt.xlabel("z [nm]")
+plt.ylabel("difference [pN]")
+plt.title("difference between calculations")
 plt.show()
 
 # %% [markdown]
@@ -207,11 +211,11 @@ plt.show()
 # %%
 an, bn = bead.ab_coeffs()
 plt.figure(figsize=(8, 6))
-plt.semilogy(range(1, an.size + 1), np.abs(an), label='$a_n$')
-plt.semilogy(range(1, bn.size + 1), np.abs(bn), label='$b_n$')
-plt.xlabel('Order')
-plt.ylabel('|$a_n$|, $|b_n|$ [-]')
-plt.title(f'Magnitude of scattering coefficients for {bead.bead_diameter * 1e6:.2f} µm bead')
+plt.semilogy(range(1, an.size + 1), np.abs(an), label="$a_n$")
+plt.semilogy(range(1, bn.size + 1), np.abs(bn), label="$b_n$")
+plt.xlabel("Order")
+plt.ylabel("|$a_n$|, $|b_n|$ [-]")
+plt.title(f"Magnitude of scattering coefficients for {bead.bead_diameter * 1e6:.2f} µm bead")
 plt.legend()
 plt.grid()
 plt.show()
