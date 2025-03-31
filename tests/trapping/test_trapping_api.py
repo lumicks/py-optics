96b5d31105af5653d98de9ea3106f06be6648f61
Robert Moerland <r.moerland@lumicks.com>
2025-03-31 22:59:05 +0200

    slightly more elegant API pytest

--- tests/trapping/test_trapping_api.py
+++ tests/trapping/test_trapping_api.py
@@ -17,10 +17,6 @@ bead = trp.Bead(bead_diameter=bead_diam, n_bead=n_bead, n_medium=n_wrong_medium)
 objective = trp.Objective(NA=NA, focal_length=focal_length, n_bfp=n_bfp, n_medium=n_medium)
 
 
-def dummy(*args):
-    pass  # We should never get there
-
-
 # Test bead immersion medium and objective medium have to be the same
 @pytest.mark.parametrize(
     "function",
@@ -36,4 +32,31 @@ def test_throw_on_wrong_medium(function) -> None:
         ValueError,
         match=re.escape("The immersion medium of the bead and the objective have to be the same"),
     ):
-        function(dummy, objective=objective, bead=bead)
+        function(lambda: None, objective=objective, bead=bead)
+
+
+@pytest.mark.parametrize(
+    "NA, focal_length, n_medium, n_bfp, error_msg",
+    [
+        (1.1, 4.43e-3, 1.0, 1.0, "The NA of the objective cannot be larger than n_medium"),
+        (
+            -1.9,
+            4.43e-3,
+            -1.0,
+            1.0,
+            "Only positive refractive indices are supported for n_bfp and n_medium",
+        ),
+        (
+            0.9,
+            4.43e-3,
+            1.0,
+            -1.0,
+            "Only positive refractive indices are supported for n_bfp and n_medium",
+        ),
+        (1.0, 0.0, 1.0, 1.0, "focal_length needs to be strictly positive"),
+        (0.0, 4.43e-3, 1.0, 1.0, "NA needs to be strictly positive"),
+    ],
+)
+def test_objective_value_errors(NA, focal_length, n_medium, n_bfp, error_msg):
+    with pytest.raises(ValueError, match=re.escape(error_msg)):
+        trp.Objective(NA=NA, focal_length=focal_length, n_bfp=n_bfp, n_medium=n_medium)
