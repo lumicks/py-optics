38ca795969ed73a9adad7b4af9ef306e08bd3ab3
Robert Moerland <r.moerland@lumicks.com>
2025-05-24 01:01:06 +0200

    use lebedev_rule from scipy

--- tests/mathutils/test_lebedev_laikov.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-import lumicks.pyoptics.mathutils.lebedev_laikov as ll
-
-
-@pytest.mark.parametrize("order", (-1, 0, 132.2))
-def test_lebedev_laikov_args(order):
-    with pytest.raises(ValueError, match=f"A value of {order} is not supported"):
-        ll.get_nearest_order(order)
